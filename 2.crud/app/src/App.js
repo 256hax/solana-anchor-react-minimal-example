00c4f6927cf0a6de81f5f255f9a681d194b3ad16
256hax <256hax@git>
2021-12-14 22:55:16 +0900

    feat: add CRUD

---
+++ 2.crud/app/src/App.js
@@ -0,0 +1,121 @@
+import './App.css';
+import { Connection, PublicKey } from '@solana/web3.js';
+import { Program, Provider, web3, BN } from '@project-serum/anchor';
+import idl from './idl.json';
+
+const { SystemProgram, Keypair } = web3;
+/* create an account  */
+const myAccount = Keypair.generate();
+const opts = {
+  preflightCommitment: "processed"
+}
+const programID = new PublicKey(idl.metadata.address);
+
+
+function App() {
+  const wallet = window.solana;
+
+  async function connectWallet() {
+    try {
+        const resp = await window.solana.connect();
+        console.log("Conneted! Public Key: ", resp.publicKey.toString());
+    } catch (err) {
+        console.log(err);
+        // => { code: 4001, message: 'User rejected the request.' }
+    }
+  }
+
+  async function disconnectWallet() {
+    window.solana.disconnect();
+    window.solana.on('disconnect', () => console.log("Disconnected!"));
+  }
+
+  async function getProvider() {
+    const network = "http://127.0.0.1:8899";
+    const connection = new Connection(network, opts.preflightCommitment);
+
+    const provider = new Provider(
+      connection, wallet, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  async function create() {
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    const data = new BN(1234);
+
+    try {
+      // Call function of programs/initialize/src/lib.rs
+      const tx = await program.rpc.create(data, {
+        accounts: {
+          myAccount: myAccount.publicKey,
+          user: provider.wallet.publicKey.toString(),
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [myAccount],
+      });
+      console.log("Sent! Signature: ", tx);
+    } catch (err) {
+      console.log("Transaction Error: ", err);
+    }
+  }
+
+  async function read() {
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    console.log(account.data.toString());
+  }
+
+  async function update() {
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+    const data = new BN(4321);
+
+    try {
+      // Call function of programs/initialize/src/lib.rs
+      const tx = await program.rpc.update(data, {
+        accounts: {
+          myAccount: myAccount.publicKey,
+        },
+      });
+      console.log("Sent! Signature: ", tx);
+    } catch (err) {
+      console.log("Transaction Error: ", err);
+    }
+  }
+
+  async function deleteDataMasking() {
+    const provider = await getProvider();
+    const program = new Program(idl, programID, provider);
+
+    try {
+      // Call function of programs/initialize/src/lib.rs
+      const tx = await program.rpc.delete({
+        accounts: {
+          myAccount: myAccount.publicKey,
+        },
+      });
+      console.log("Sent! Signature: ", tx);
+    } catch (err) {
+      console.log("Transaction Error: ", err);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={connectWallet}>1. Connect to Wallet</button>
+        <button onClick={create}>2. Create</button>
+        <button onClick={read}>3. Read</button>
+        <button onClick={update}>4. Update</button>
+        <button onClick={deleteDataMasking}>5. Delete(Data Masking)</button>
+        <button onClick={disconnectWallet}>6. Disconnect</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2.crud/programs/crud/src/lib.rs
@@ -0,0 +1,49 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9Y88G9ZfugSDD24mCBpe64KAiEkaxieVPZYgVkrYR3gz");
+
+#[program]
+mod crud {
+    use super::*;
+
+    pub fn create(ctx: Context<Initialize>, data: u64) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        msg!("data: {}", data);
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        msg!("data: {}", data);
+        Ok(())
+    }
+
+    pub fn delete(ctx: Context<Update>) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = 0;
+        msg!("data: {}", my_account.data);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub data: u64,
+}
