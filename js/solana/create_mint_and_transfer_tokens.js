d8a03b889f787a1d42aca5b99f744b38a1543dc5
256hax <256hax@git>
2022-03-12 10:59:49 +0900

    fix: convert ts -> js

--- js/solana/create_mint_and_transfer_tokens.js
+++ js/solana/create_mint_and_transfer_tokens.js
@@ -6,11 +6,11 @@ const splToken = require('@solana/spl-token');
 (async () => {
     // Connect to cluster
     // const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
-    const connection = new web3.Connection(web3.clusterApiUrl('http://localhost:8899'), 'confirmed');
+    const connection = new web3.Connection('http://localhost:8899', 'confirmed');
 
     // Generate a new wallet keypair and airdrop SOL
     const fromWallet = web3.Keypair.generate();
-    const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, LAMPORTS_PER_SOL);
+    const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, web3.LAMPORTS_PER_SOL);
 
     // Wait for airdrop confirmation
     await connection.confirmTransaction(fromAirdropSignature);
@@ -19,10 +19,10 @@ const splToken = require('@solana/spl-token');
     const toWallet = web3.Keypair.generate();
 
     // Create new token mint
-    const mint = await createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
+    const mint = await splToken.createMint(connection, fromWallet, fromWallet.publicKey, null, 9);
 
     // Get the token account of the fromWallet address, and if it does not exist, create it
-    const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+    const fromTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(
         connection,
         fromWallet,
         mint,
@@ -30,35 +30,40 @@ const splToken = require('@solana/spl-token');
     );
 
     // Get the token account of the toWallet address, and if it does not exist, create it
-    const toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey);
+    const toTokenAccount = await splToken.getOrCreateAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey);
 
     // Mint 1 new token to the "fromTokenAccount" account we just created
-    let signature = await mintTo(
+    const signature_mint = await splToken.mintTo(
         connection,
         fromWallet,
         mint,
         fromTokenAccount.address,
         fromWallet.publicKey,
-        1000000000,
+        web3.LAMPORTS_PER_SOL,
         []
     );
-    console.log('mint tx:', signature);
 
     // Transfer the new token to the "toTokenAccount" we just created
-    signature = await transfer(
+    const signature_transfer = await splToken.transfer(
         connection,
         fromWallet,
         fromTokenAccount.address,
         toTokenAccount.address,
         fromWallet.publicKey,
-        1000000000,
+        web3.LAMPORTS_PER_SOL,
         []
     );
-    console.log('transfer tx:', signature);
+
+    console.log('fromWallet.publicKey =>', fromWallet.publicKey.toString());
+    console.log('toWallet.publicKey   =>', toWallet.publicKey.toString());
+    console.log('mint tx              =>', signature_mint);
+    console.log('transfer txx         =>', signature_transfer);
 })();
+
 /*
 % node <THIS JS FILE>
-fromWallet Publickey ->  5b14DdMF2HuXEHCpjUQQnupZJW4LtvsUhFL3Vs4LcBHR
-toWallet Publickey ->  LzSU5qwoAQd8CJwbcqHgtZqBtpzWsxGPwjc9Y7aj1FY
-Signature -> 27bWC2Bdagk1PxczcenPf13fsXxYmJ8ZP3YygAmUPJLe27ySi4XksryJFBfHRE4hYtwrJxzo43BzC4dCMRyzHDAx
+fromWallet.publicKey => Cu8zkHdqqfhHRSutTxbyM5t4iYPM1NLxL9Koz6pUZp5V
+toWallet.publicKey   => 8MqL5e8iZwrzP4CVbkNXZfm77VKxm6vUjaaUp78vA5Yf
+mint tx              => 3FQikxc583Cnt4vv3RUfS6VhR5zPwXbhWyAUuV7cYMumJaMjLKTNHUTUzVLMUti7UNbDkTcUHa9U1RfjgULLhc7h
+transfer txx         => 4jGURs2YZATrp7Ztxo5EhRTiDgGoLWjNHsWtEK4V5GzS63zYHe26Udnxx4WY61pNctqNFfQuxPKHfU8WEJXxzDxB
 */
