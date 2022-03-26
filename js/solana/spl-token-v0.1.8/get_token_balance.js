fee055de7211f53edd2de96dd89299828d4f3d90
256hax <256hax@git>
2022-03-26 15:32:35 +0900

    feat: add get token balance

---
+++ js/solana/spl-token-v0.1.8/get_token_balance.js
@@ -0,0 +1,98 @@
+// Source: https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getBalance
+const web3 = require("@solana/web3.js");
+var splToken = require('@solana/spl-token');
+
+async function main() {
+  let myPubkey = web3.Keypair.generate();
+  let connection = new web3.Connection('http://localhost:8899', 'confirmed');
+
+  console.log('myPubkey =>', myPubkey.publicKey.toString());
+
+
+  let airdropSignature = await connection.requestAirdrop(
+      myPubkey.publicKey,
+      web3.LAMPORTS_PER_SOL,
+  );
+  await connection.confirmTransaction(airdropSignature);
+
+
+  const mint = await splToken.Token.createMint(
+    connection,
+    myPubkey,                 // mint_pubkey: &Pubkey,
+    myPubkey.publicKey,       // mint_authority_pubkey: &Pubkey,
+    null,                       // freeze_authority_pubkey: Option<&Pubkey>,
+    9,                          // decimals: u8,
+    splToken.TOKEN_PROGRAM_ID,  // token_program_id: &Pubkey,
+  );
+
+  //get the token account of the fromWallet Solana address, if it does not exist, create it
+  const myTokenPubkey = await mint.getOrCreateAssociatedAccountInfo(
+    myPubkey.publicKey,
+  );
+
+  console.log('\n--- Create Token Account ---');
+  console.log('myTokenPubkey =>', myTokenPubkey);
+
+
+  console.log('\n--- Token Account Balance ---');
+  // Ref: https://solana-labs.github.io/solana-web3.js/classes/Connection.html#getTokenAccountBalance
+  const myTokenPubkeyBalance = await connection.getTokenAccountBalance(myTokenPubkey.address);
+  console.log('myTokenPubkeyBalance.value.amount =>', myTokenPubkeyBalance.value.amount);
+
+
+  console.log('\n--- Mint token ---');
+  const mint_tx = await mint.mintTo(
+    myTokenPubkey.address, // account_pubkey(destination): &Pubkey,
+    myPubkey.publicKey,     // owner_pubkey(mint_authority_pubkey): &Pubkey,
+    [],                       // signer_pubkeys: &[&Pubkey],
+    web3.LAMPORTS_PER_SOL,               // amount: u64,
+  );
+
+  console.log('Minted LAMPORTS_PER_SOL tokens!');
+
+  console.log('\n--- Token Account Balance ---');
+  const myTokenPubkeyBalanceMinted = await connection.getTokenAccountBalance(myTokenPubkey.address);
+  console.log('myTokenPubkeyBalanceMinted.value.amount =>', myTokenPubkeyBalanceMinted.value.amount);
+
+}
+
+main();
+
+/*
+% node <THIS JS FILE>
+myPubkey => DKpXE8EE9XYP1nesLL8nmNcPiBP3LsYgzUh4Uwct1xfo
+
+--- Create Token Account ---
+myTokenPubkey => {
+  mint: PublicKey {
+    _bn: <BN: e4cdaea68467e1bcdf2f314868858c0333428bb92287d9b610ad56077e714fe8>
+  },
+  owner: PublicKey {
+    _bn: <BN: b71e6e14f26fae8eff8215c7cba7a11997fcf5d83e64796897231629bb60ae96>
+  },
+  amount: <BN: 0>,
+  delegateOption: 0,
+  delegate: null,
+  state: 1,
+  isNativeOption: 0,
+  isNative: false,
+  delegatedAmount: <BN: 0>,
+  closeAuthorityOption: 0,
+  closeAuthority: null,
+  address: PublicKey {
+    _bn: <BN: ea984a07545b2c317c0483eb4e99503dde66b46e35670de29d7287e7bbf9a235>
+  },
+  isInitialized: true,
+  isFrozen: false,
+  rentExemptReserve: null
+}
+
+--- Token Account Balance ---
+myTokenPubkeyBalance.value.amount => 0
+
+--- Mint token ---
+Minted LAMPORTS_PER_SOL tokens!
+
+--- Token Account Balance ---
+myTokenPubkeyBalanceMinted.value.amount => 1000000000
+*/
