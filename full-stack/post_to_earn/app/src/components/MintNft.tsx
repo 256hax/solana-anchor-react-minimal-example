db8ae59356811c905c4107e21886699eae6b2976
256hax <256hax@git>
2022-04-16 23:40:00 +0900

    dev: feat: use AnchorProvider

--- full-stack/post_to_earn/app/src/components/MintNft.tsx
+++ full-stack/post_to_earn/app/src/components/MintNft.tsx
@@ -1,16 +1,28 @@
 import React, { useState, useContext } from 'react';
 import { actions, utils, programs, NodeWallet} from '@metaplex/js';
 import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { Program, BN, IdlAccounts, AnchorProvider } from "@project-serum/anchor";
 import { arTransactionIdContext } from '../providers/ArTransactionId';
 
-export function MintNft() {
-  const { valArTransactionId } = useContext(arTransactionIdContext);
+// For "Property 'solana' does not exist on type 'Window & typeof globalThis'" error.
+interface Window {
+  solana: any
+}
+declare var window: Window
 
-  async function testDeleteLater(){
-    console.log(valArTransactionId);
+export function MintNft() {
+  async function getProvider() {
+    const network = "http://127.0.0.1:8899";
+    const connection = new Connection(network, 'processed');
+    const wallet = window.solana;
+
+    const provider = new AnchorProvider(
+      connection, wallet, { commitment: 'processed' }
+    );
+    return provider;
   }
 
-  return (
-    <button onClick={testDeleteLater}>test</button>
+  return(
+    <> </>
   );
 }
