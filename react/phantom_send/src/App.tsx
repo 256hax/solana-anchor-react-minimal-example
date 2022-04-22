99ea626f8a5082a48d8667c7af0790420d8041dd
256hax <256hax@git>
2022-04-22 23:23:37 +0900

    ref: change directory

--- react/phantom_send/src/App.tsx
+++ react/phantom_send/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Wallet } from './Wallet';
+import { Wallet } from './components/Wallet';
 
 function App() {
   return (
