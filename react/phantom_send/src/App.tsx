96bb086d3f819caa27ede75d01c0b1cc3bcc011d
256hax <256hax@git>
2022-04-22 23:12:02 +0900

    init: add adapter

--- react/phantom_send/src/App.tsx
+++ react/phantom_send/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Wallet } from './Wallet';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Wallet />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
