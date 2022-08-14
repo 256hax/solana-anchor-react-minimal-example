be84f7eb54e6c57ab8a71bc2d1de77ea391445e1
256hax <256hax@git>
2022-08-14 21:04:55 +0900

    feat: add secretkey

--- anchor/guess_the_number/app/modules/initializeWallets.ts
+++ anchor/guess_the_number/app/modules/initializeWallets.ts
@@ -1,29 +1,32 @@
 import { PublicKey, Keypair, SystemProgram, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import * as fs from 'fs';
 import { sleep } from 'sleep';
 
 export const initializeWallets = async(connection: any) => {
-  const taker1 = Keypair.generate();
-  const taker2 = Keypair.generate();
+  const secretKey1 = new Uint8Array(JSON.parse(fs.readFileSync('./app/assets/keys/taker1.key.json', 'utf8')));
+  const taker1 = Keypair.fromSecretKey(secretKey1);
 
-  const wallets = [taker1, taker2];
+  const secretKey2 = new Uint8Array(JSON.parse(fs.readFileSync('./app/assets/keys/taker2.key.json', 'utf8')));
+  const taker2 = Keypair.fromSecretKey(secretKey2);
 
-  let latestBlockHash: any;
-  let airdropSignature: any;
 
-  for(const w of wallets) {
-    // Generate a new wallet keypair and airdrop SOL
-    airdropSignature = await connection.requestAirdrop(w.publicKey, LAMPORTS_PER_SOL);
-    latestBlockHash = await connection.getLatestBlockhash();
+  // const wallets = [taker1, taker2];
+  // let latestBlockHash: any;
+  // let airdropSignature: any;
 
-    // Wait for airdrop confirmation
-    await connection.confirmTransaction({
-      blockhash: latestBlockHash.blockhash,
-      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: airdropSignature,
-    })
+  // // Notice: Use following for only localnet
+  // for(const w of wallets) {
+  //   // Generate a new wallet keypair and airdrop SOL
+  //   airdropSignature = await connection.requestAirdrop(w.publicKey, LAMPORTS_PER_SOL);
+  //   latestBlockHash = await connection.getLatestBlockhash();
 
-    sleep(1); // 1 = 1sec. for too many request
-  };
+  //   // Wait for airdrop confirmation
+  //   await connection.confirmTransaction({
+  //     blockhash: latestBlockHash.blockhash,
+  //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+  //     signature: airdropSignature,
+  //   })
+  // }
 
   return [taker1, taker2];
 };
