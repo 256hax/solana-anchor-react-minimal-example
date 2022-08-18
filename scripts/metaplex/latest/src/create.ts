a1940307072e7232242644f8820195c32e3f57c6
256hax <256hax@git>
2022-08-18 11:28:14 +0900

    docs: add console.log

--- scripts/metaplex/latest/src/create.ts
+++ scripts/metaplex/latest/src/create.ts
@@ -62,3 +62,69 @@ const main = async() => {
 };
 
 main();
+
+/*
+% ts-node <THIS FILE>
+uri => https://arweave.net/bnKB2a0Xix2ilJYbbok8l1O6xf0EVjkk_hdGGYyAe5s
+nft => {
+  model: 'nft',
+  lazy: false,
+  address: Pda {
+    _bn: <BN: 522c861d165388178021fc4b96abcab45407ed42100807403658a1e339a0ad97>,
+    bump: 255
+  },
+  mintAddress: PublicKey {
+    _bn: <BN: 610893d740710147d66b9864e8caae009106097c6f4a3052e93d325fac4bb01b>
+  },
+  updateAuthorityAddress: PublicKey {
+    _bn: <BN: 299bc9eb6c1857b02c7963e184022d06164480392545f8a8fdf5d33b2c5aa708>
+  },
+  name: 'My NFT',
+  symbol: '',
+  uri: 'https://arweave.net/bnKB2a0Xix2ilJYbbok8l1O6xf0EVjkk_hdGGYyAe5s',
+  isMutable: true,
+  primarySaleHappened: false,
+  sellerFeeBasisPoints: 500,
+  editionNonce: 253,
+  creators: [ { address: [PublicKey], verified: true, share: 100 } ],
+  tokenStandard: 0,
+  collection: null,
+  uses: null,
+  json: {
+    name: 'My NFT Metadata',
+    description: 'My description',
+    image: 'https://placekitten.com/200/300'
+  },
+  metadataAddress: Pda {
+    _bn: <BN: 522c861d165388178021fc4b96abcab45407ed42100807403658a1e339a0ad97>,
+    bump: 255
+  },
+  mint: {
+    model: 'mint',
+    address: PublicKey {
+      _bn: <BN: 610893d740710147d66b9864e8caae009106097c6f4a3052e93d325fac4bb01b>
+    },
+    mintAuthorityAddress: PublicKey {
+      _bn: <BN: bd910a9f0d8dc41a04702b7fcddc52474e15a971534bbaefdc0313f4938a8faa>
+    },
+    freezeAuthorityAddress: PublicKey {
+      _bn: <BN: bd910a9f0d8dc41a04702b7fcddc52474e15a971534bbaefdc0313f4938a8faa>
+    },
+    decimals: 0,
+    supply: { basisPoints: <BN: 1>, currency: [Object] },
+    isWrappedSol: false,
+    currency: { symbol: 'Token', decimals: 0, namespace: 'spl-token' }
+  },
+  edition: {
+    model: 'nftEdition',
+    isOriginal: true,
+    address: Pda {
+      _bn: <BN: bd910a9f0d8dc41a04702b7fcddc52474e15a971534bbaefdc0313f4938a8faa>,
+      bump: 253
+    },
+    supply: <BN: 0>,
+    maxSupply: <BN: 1>
+  }
+}
+Mint Address => 7Xn8hJYa9Cd64FJ5BzBZmAsxYtfPhutMvKf2YwwZ7W2W
+*/
