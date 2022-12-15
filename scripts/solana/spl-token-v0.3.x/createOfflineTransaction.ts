d4d36005328bf1aa4d4f8c977f0edc8f5c953b92
256hax <256hax@gmail.com>
2022-12-15 18:46:46 +0900

    ref: change to const

--- scripts/solana/spl-token-v0.3.x/createOfflineTransaction.ts
+++ scripts/solana/spl-token-v0.3.x/createOfflineTransaction.ts
@@ -51,7 +51,7 @@ import * as bs58 from "bs58";
   });
 
   // ------------------------------------------------------------------------
-  // 1. Create Transaction
+  //  1. Create Transaction
   // ------------------------------------------------------------------------
   let tx = new Transaction().add(
     SystemProgram.transfer({
@@ -66,32 +66,32 @@ import * as bs58 from "bs58";
   tx.lastValidBlockHeight = latestBlockhash.lastValidBlockHeight;
 
   tx.feePayer = feePayer.publicKey;
-  let realDataNeedToSign = tx.serializeMessage(); // the real data singer need to sign.
+  const realDataNeedToSign = tx.serializeMessage(); // the real data singer need to sign.
 
   // ------------------------------------------------------------------------
-  // 2. Sign Transaction
+  //  2. Sign Transaction
   // ------------------------------------------------------------------------
   // use any lib you like, the main idea is to use ed25519 to sign it.
   // the return signature should be 64 bytes.
-  let feePayerSignature = nacl.sign.detached(
+  const feePayerSignature = nacl.sign.detached(
     realDataNeedToSign,
     feePayer.secretKey
   );
-  let aliceSignature = nacl.sign.detached(realDataNeedToSign, alice.secretKey);
+  const aliceSignature = nacl.sign.detached(realDataNeedToSign, alice.secretKey);
 
   // ------------------------------------------------------------------------
-  // 3. Recover Tranasction
+  //  3. Recover Tranasction
   // ------------------------------------------------------------------------
 
   // you can verify signatures before you recovering the transaction
-  let verifyFeePayerSignatureResult = nacl.sign.detached.verify(
+  const verifyFeePayerSignatureResult = nacl.sign.detached.verify(
     realDataNeedToSign,
     feePayerSignature,
     feePayer.publicKey.toBytes() // you should use the raw pubkey (32 bytes) to verify
   );
   console.log(`verify feePayer signature: ${verifyFeePayerSignatureResult}`);
 
-  let verifyAliceSignatureResult = nacl.sign.detached.verify(
+  const verifyAliceSignatureResult = nacl.sign.detached.verify(
     realDataNeedToSign,
     aliceSignature,
     alice.publicKey.toBytes()
@@ -102,7 +102,7 @@ import * as bs58 from "bs58";
   // there are two ways you can recover the tx
 
   // ------------------------------------------------------------------------
-  // 3.a. Recover Tranasction (use populate then addSignauture)
+  //  3.a. Recover Tranasction (use populate then addSignauture)
   // ------------------------------------------------------------------------
   {
     let recoverTx = Transaction.populate(Message.from(realDataNeedToSign));
@@ -110,7 +110,7 @@ import * as bs58 from "bs58";
     recoverTx.addSignature(alice.publicKey, Buffer.from(aliceSignature));
 
     // ------------------------------------------------------------------------
-    // 4. Send transaction
+    //  4. Send transaction
     // ------------------------------------------------------------------------
     console.log(
       `Case a(populate then sign): txhash => ${await connection.sendRawTransaction(recoverTx.serialize())}`
@@ -120,7 +120,7 @@ import * as bs58 from "bs58";
   // or
 
   // ------------------------------------------------------------------------
-  // 3.b. Recover Tranasction (use populate with signature)
+  //  3.b. Recover Tranasction (use populate with signature)
   // ------------------------------------------------------------------------
   {
     let recoverTx = Transaction.populate(Message.from(realDataNeedToSign), [
@@ -129,7 +129,7 @@ import * as bs58 from "bs58";
     ]);
 
     // ------------------------------------------------------------------------
-    // 4. Send transaction
+    //  4. Send transaction
     // ------------------------------------------------------------------------
     console.log(
       `Case b(populate with sign): txhash => ${await connection.sendRawTransaction(recoverTx.serialize())}`
