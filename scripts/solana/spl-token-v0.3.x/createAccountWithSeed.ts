7f048400eeee02149a10a1b769ba0bc19127b92f
256hax <256hax@gmail.com>
2022-12-25 14:01:51 +0900

    ref: for consistency

--- scripts/solana/spl-token-v0.3.x/createAccountWithSeed.ts
+++ scripts/solana/spl-token-v0.3.x/createAccountWithSeed.ts
@@ -20,9 +20,7 @@ export const main = async () => {
   // ------------------------------------------------------------------------
   // 5YNmS1R9nNSCDzb5a7mMJ1dwK9uHeAAF4CmPEwKgVWr8
   const feePayer = Keypair.fromSecretKey(
-    bs58.decode(
-      "588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2"
-    )
+    bs58.decode('588FU4PktJWfGfxtzpAAXywSNt74AvtroVzGfKkVN1LwRuvHwKGr851uH8czM5qm4iqLbs1kKoMKtMJG4ATR7Ld2')
   );
 
   // // G2FAbFQPFa5qKXCetoFZQEvF9BVvCKbvUZvodpVidnoY
