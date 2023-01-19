1e672f67d046f5cb9ffd0cb0ef1b451737a0b545
256hax <256hax@gmail.com>
2023-01-19 11:57:07 +0900

    feat: add search transaction

--- scripts/solana/spl-token-v0.3.x/createTransferInstruction.ts
+++ scripts/solana/spl-token-v0.3.x/createTransferInstruction.ts
@@ -64,34 +64,47 @@ export const main = async () => {
     [payer]
   );
 
+  // ------------------------------------------
+  //  Search Signature
+  // ------------------------------------------
+  const signaturesByAddress = await connection.getConfirmedSignaturesForAddress2(reference.publicKey);
+  const parsedSignature = await connection.getParsedTransaction(signaturesByAddress[0].signature);
+  
+
+  console.log('\n--- Excute Transaction ---');
   console.log('payer => ', payer.publicKey.toString());
   console.log('reference => ', reference.publicKey.toString());
   console.log('taker => ', taker.publicKey.toString());
   console.log('transferInstruction =>',  transferInstruction);
   console.log('Signature => ', signature);
+  console.log('\n--- Search Transaction ---');
+  console.log('signaturesByAddress => ', signaturesByAddress);
+  console.log('parsedSignature => ', parsedSignature);
 };
 
 main();
 
 /*
 % ts-node <THIS JS FILE>
-payer =>  28CneegKU1BiR2ArpoWtCykVJT1Sscd2yESZwpRQermG
-reference =>  8soaoi7xSwQDn4JbUv9Drz959j2qv1h6p9oZ1TPjnvtQ
-taker =>  CYWTbrxrAx8yUCqqZ8bdxxreWwR7qzr1yeHtnSW3ceNj
+
+--- Excute Transaction ---
+payer =>  Ak6NdaUWqr4niNPYKsUe9LCiiaytTmWYHbu3CvKEgp8Z
+reference =>  ZSorocWCiSBUckZVt5hqd3Ff744RmsKzzNxLLwZcg3d
+taker =>  3Yg4CyJT1EkNx96sgaKf5iiXF5715qMm4HAHJxFVkX1A
 transferInstruction => TransactionInstruction {
   keys: [
     {
-      pubkey: [PublicKey [PublicKey(28CneegKU1BiR2ArpoWtCykVJT1Sscd2yESZwpRQermG)]],
+      pubkey: [PublicKey [PublicKey(Ak6NdaUWqr4niNPYKsUe9LCiiaytTmWYHbu3CvKEgp8Z)]],
       isSigner: true,
       isWritable: true
     },
     {
-      pubkey: [PublicKey [PublicKey(CYWTbrxrAx8yUCqqZ8bdxxreWwR7qzr1yeHtnSW3ceNj)]],
+      pubkey: [PublicKey [PublicKey(3Yg4CyJT1EkNx96sgaKf5iiXF5715qMm4HAHJxFVkX1A)]],
       isSigner: false,
       isWritable: true
     },
     {
-      pubkey: [PublicKey [PublicKey(8soaoi7xSwQDn4JbUv9Drz959j2qv1h6p9oZ1TPjnvtQ)]],
+      pubkey: [PublicKey [PublicKey(ZSorocWCiSBUckZVt5hqd3Ff744RmsKzzNxLLwZcg3d)]],
       isWritable: false,
       isSigner: false
     }
@@ -101,5 +114,50 @@ transferInstruction => TransactionInstruction {
   },
   data: <Buffer 02 00 00 00 40 42 0f 00 00 00 00 00>
 }
-Signature =>  3iK8vQiefcdy7GHW7yykkmtZc2yy1PKSvsiBiZRx9BV5VH6NzfmRRvCibyGJezfjYQRLcXDsRAx3UG3R9sU3z4Ab
+Signature =>  3vqKs1JWnpSw2FAjU1qQQWiSoeDSFFdyJGgxJB7ocE1oPgq9o5WgLk4XWyGMduanNhcAdUNJz9B3moa3CPzQSsv2
+
+--- Search Transaction ---
+signaturesByAddress =>  [
+  {
+    blockTime: 1674096985,
+    confirmationStatus: 'confirmed',
+    err: null,
+    memo: null,
+    signature: '3vqKs1JWnpSw2FAjU1qQQWiSoeDSFFdyJGgxJB7ocE1oPgq9o5WgLk4XWyGMduanNhcAdUNJz9B3moa3CPzQSsv2',
+    slot: 10501
+  }
+]
+parsedSignature =>  {
+  blockTime: 1674096985,
+  meta: {
+    computeUnitsConsumed: 0,
+    err: null,
+    fee: 5000,
+    innerInstructions: [],
+    logMessages: [
+      'Program 11111111111111111111111111111111 invoke [1]',
+      'Program 11111111111111111111111111111111 success'
+    ],
+    postBalances: [ 998995000, 1000000, 1, 0 ],
+    postTokenBalances: [],
+    preBalances: [ 1000000000, 0, 1, 0 ],
+    preTokenBalances: [],
+    rewards: [],
+    status: { Ok: null },
+    loadedAddresses: undefined
+  },
+  slot: 10501,
+  transaction: {
+    message: {
+      accountKeys: [Array],
+      addressTableLookups: null,
+      instructions: [Array],
+      recentBlockhash: '4CtBaweswY6i4zYXNrMTMus3wst4CW29tXTzCxzvEH9Y'
+    },
+    signatures: [
+      '3vqKs1JWnpSw2FAjU1qQQWiSoeDSFFdyJGgxJB7ocE1oPgq9o5WgLk4XWyGMduanNhcAdUNJz9B3moa3CPzQSsv2'
+    ]
+  },
+  version: undefined
+}
 */
