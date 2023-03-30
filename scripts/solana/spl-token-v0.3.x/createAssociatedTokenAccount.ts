55513e87d66249e87b5cb3cdcfaf9b1f7694adc8
256hax <256hax@gmail.com>
2023-03-30 23:28:34 +0900

    ref: merge createTokenAccount into createAssociatedTokenAccount file
    create ATA in @solana/spl-token has two ways:
    1. createAccount,
    2. createAssociatedTokenAccount,

--- scripts/solana/spl-token-v0.3.x/createAssociatedTokenAccount.ts
+++ scripts/solana/spl-token-v0.3.x/createAssociatedTokenAccount.ts
@@ -3,38 +3,56 @@ import {
   Connection,
   Keypair,
   LAMPORTS_PER_SOL,
-  PublicKey
+  PublicKey,
 } from '@solana/web3.js';
-import { createAssociatedTokenAccount } from '@solana/spl-token';
+import {
+  createMint,
+  createAccount,
+  createAssociatedTokenAccount,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
 
 export const main = async () => {
-  // const connection = new Connection('http://127.0.0.1:8899', 'confirmed');
-  const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+  const connection = new Connection('http://127.0.0.1:8899', 'confirmed');
+  // const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
 
-
-  // --- Payer -------------------------------------------------------------
+  // ---------------------------------------------------
+  //  Payer
+  // ---------------------------------------------------
   const payer = Keypair.generate();
 
+  // ---------------------------------------------------
+  //  Airdrop
+  // ---------------------------------------------------
+  let latestBlockhash = await connection.getLatestBlockhash();
   const airdropSignature = await connection.requestAirdrop(
     payer.publicKey,
     LAMPORTS_PER_SOL
   );
-
-  let latestBlockhash = await connection.getLatestBlockhash();
-
   await connection.confirmTransaction({
     blockhash: latestBlockhash.blockhash,
     lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
     signature: airdropSignature,
   });
 
+  console.log('owner =>', payer.publicKey.toString());
 
-  // --- Token -------------------------------------------------------------
-  const mint = new PublicKey('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'); // USDC Token in Devnet
-
-
-  // --- Create ATA -------------------------------------------------------------
+  // ---------------------------------------------------
+  //  Create ATA using createAssociatedTokenAccount
+  // ---------------------------------------------------
   // Ref: https://solana-labs.github.io/solana-program-library/token/js/modules.html#createAssociatedTokenAccount
+
+  // const mint = new PublicKey('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'); // USDC Token in Devnet
+  const mint = await createMint(
+    connection, // connection
+    payer, // payer
+    payer.publicKey, // mintAuthority
+    null, // freezeAuthority
+    9 // decimals
+  );
+
+  // createAccount using ASSOCIATED_TOKEN_PROGRAM_ID.
   const ata = await createAssociatedTokenAccount(
     connection, // connection
     payer, // payer
@@ -42,14 +60,40 @@ export const main = async () => {
     payer.publicKey, // owner
   );
 
-  console.log('owner =>', payer.publicKey.toString());
-  console.log('Associated Token Address =>', ata.toString());
+  console.log('ata =>', ata.toString());
+  console.log('ASSOCIATED_TOKEN_PROGRAM_ID =>', ASSOCIATED_TOKEN_PROGRAM_ID.toString());
+  
+  // ---------------------------------------------------
+  //  Create ATA using createMint
+  // ---------------------------------------------------
+  // ref: https://solana-labs.github.io/solana-program-library/token/js/modules.html#createAccount
+  const mint2 = await createMint(
+    connection, // connection
+    payer, // payer
+    payer.publicKey, // mintAuthority
+    null, // freezeAuthority
+    9 // decimals
+  );
+
+  // createAccount using TOKEN_PROGRAM_ID.
+  const ata2 = await createAccount(
+    connection, // connection,
+    payer, // payer
+    mint2, // mint
+    payer.publicKey, // owner
+  );
+
+  console.log('ata2 =>', ata2.toString());
+  console.log('TOKEN_PROGRAM_ID =>', TOKEN_PROGRAM_ID.toString());
 };
 
 main();
 
 /*
 % ts-node <THIS FILE>
-owner => CoBiCaAd5cVqTWZNBecHpJHiHH3Yy5B4pY2YwyaMVhBr
-Associated Token Address => AFY2QZHYgW8hkSjiGR5PCUJJdvuTmrwNv9Pi4PDgv5yC
+owner => 9U8ZLA6jdLPSet6zm3fNB1yt3tNQufJrzjHtX8yyLwwW
+ata => 3rgM2MxWbNrVZo6QBUGXxsPbGQ1xajhskR1Ux6Wpcvbe
+ASSOCIATED_TOKEN_PROGRAM_ID => ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL
+ata2 => etWvc85VD3heS7wziHUKeuqkiQJu9S8XsPyxHR9xan7
+TOKEN_PROGRAM_ID => TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA
 */
