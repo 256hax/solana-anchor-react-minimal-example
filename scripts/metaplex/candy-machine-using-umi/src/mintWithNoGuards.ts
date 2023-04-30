4b814d957851eea2380fb9b4190a87e81a649843
256hax <256hax@gmail.com>
2023-04-30 21:16:04 +0900

    chore: rename directory

---
+++ scripts/metaplex/candy-machine-using-umi/src/mintWithNoGuards.ts
@@ -0,0 +1,249 @@
+// Docs: https://docs.metaplex.com/programs/candy-machine/
+// Test Code: https://github.com/metaplex-foundation/mpl-candy-machine/blob/main/clients/js/test/mintV2.test.ts
+
+// Lib
+import * as fs from 'fs';
+
+// Candy Machine
+import {
+  mplCandyMachine,
+  addConfigLines,
+  create,
+  mintV2,
+  fetchCandyMachine,
+} from "@metaplex-foundation/mpl-candy-machine";
+import {
+  setComputeUnitLimit,
+} from "@metaplex-foundation/mpl-essentials";
+
+// Token
+import {
+  createNft,
+  TokenStandard,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+// Umi
+import {
+  generateSigner,
+  keypairIdentity,
+  base58PublicKey,
+  transactionBuilder,
+  percentAmount,
+  some,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+
+const main = async () => {
+  const endopoint = 'https://api.devnet.solana.com';
+  const umi = createUmi(endopoint, 'confirmed')
+    .use(mplCandyMachine());
+
+  const secretKey = new Uint8Array(JSON.parse(fs.readFileSync('./assets/id.json', 'utf8')));
+  // Ref: https://github.com/metaplex-foundation/umi/blob/main/docs/publickeys-signers.md#keypairs
+  const myKeypair = umi.eddsa.createKeypairFromSecretKey(secretKey);
+  umi.use(keypairIdentity(myKeypair));
+
+  // If you build frontend using Wallet Adapter, use following.
+  // 
+  // import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
+  // const wallet = useWallet();
+  // const umi = createUmi(endopoint, 'confirmed')
+  //   .use(walletAdapterIdentity(wallet))
+  //   .use(mplCandyMachine())
+
+  // Create the Collection NFT.
+  const collectionUpdateAuthority = generateSigner(umi);
+  const collectionMint = generateSigner(umi);
+  // Ref: https://mpl-token-metadata-js-docs.vercel.app/functions/createNft.html
+  await createNft(umi, {
+    mint: collectionMint,
+    authority: collectionUpdateAuthority,
+    name: "My Collection NFT",
+    uri: "https://arweave.net/yfVoS8kmFiM_XjfZOETgdCfrByKDyheSJ20nyam8_ag",
+    sellerFeeBasisPoints: percentAmount(9.99, 2), // 9.99%
+    isCollection: true,
+  }).sendAndConfirm(umi);
+
+  // Create a Candy Machine with guards.
+  // Ref: https://docs.metaplex.com/programs/candy-machine/candy-machine-settings
+  const candyMachine = generateSigner(umi);
+  const createInstructions = await create(umi, {
+    candyMachine,
+    collectionMint: collectionMint.publicKey,
+    collectionUpdateAuthority,
+    tokenStandard: TokenStandard.NonFungible,
+    sellerFeeBasisPoints: percentAmount(9.99, 2), // 9.99%
+    itemsAvailable: 3, // Increase SOL cost per items. Check the cost on Devnet before launch.
+    creators: [
+      {
+        address: umi.identity.publicKey,
+        verified: true,
+        percentageShare: 100,
+      },
+    ],
+    configLineSettings: some({
+      prefixName: "",
+      nameLength: 32,
+      prefixUri: "",
+      uriLength: 200,
+      // isSequential: indicates to whether a sequential index generation should be used during mint or not (recommended to set this value to false).
+      isSequential: false,
+    }),
+  });
+  await transactionBuilder().add(createInstructions).sendAndConfirm(umi);
+
+  // Inseting Items.
+  // Ref: https://docs.metaplex.com/programs/candy-machine/inserting-items
+  await addConfigLines(umi, {
+    candyMachine: candyMachine.publicKey,
+    index: 0,
+    configLines: [
+      { name: "My NFT #1", uri: "https://arweave.net/2V5Mx2dwnyrwlpHYPVshiyj_WtU7qIlPZzKs6nIwqw4" },
+      { name: "My NFT #2", uri: "https://arweave.net/2dyAzm_p5kz8GYYLAzKj41KQL3vYT_kWyomJ3mTpBcA" },
+      { name: "My NFT #3", uri: "https://arweave.net/tD5TVplWyzqYvM9feLbIKGqkuTzf0AIO0nLc4NDiiPk" },
+    ],
+  }).sendAndConfirm(umi);
+
+  // First Minting.
+  const nftMint = generateSigner(umi);
+  await transactionBuilder()
+    .add(setComputeUnitLimit(umi, { units: 800_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine: candyMachine.publicKey,
+        nftMint,
+        collectionMint: collectionMint.publicKey,
+        collectionUpdateAuthority: collectionUpdateAuthority.publicKey,
+      }),
+    )
+    .sendAndConfirm(umi);
+
+  // Second Minting.
+  const nftMint2 = generateSigner(umi);
+  await transactionBuilder()
+    .add(setComputeUnitLimit(umi, { units: 800_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine: candyMachine.publicKey,
+        nftMint: nftMint2,
+        collectionMint: collectionMint.publicKey,
+        collectionUpdateAuthority: collectionUpdateAuthority.publicKey,
+      }),
+    )
+    .sendAndConfirm(umi);
+
+  // Fetch Candy Machine.
+  const candyMachineAccount = await fetchCandyMachine(
+    umi,
+    candyMachine.publicKey
+  );
+
+  // Ref: https://github.com/metaplex-foundation/umi/blob/main/docs/publickeys-signers.md#public-keys
+  console.log('myKeypair.publicKey =>', base58PublicKey(myKeypair.publicKey.bytes));
+  console.log('collectionUpdateAuthority =>', base58PublicKey(collectionUpdateAuthority.publicKey));
+  console.log('collectionMint =>', base58PublicKey(collectionMint.publicKey));
+  console.log('candyMachine =>', base58PublicKey(candyMachine.publicKey));
+  console.log('nftMint =>', base58PublicKey(nftMint.publicKey));
+  console.log('-------------------------------------------------------');
+  console.log('candyMachineAccount =>', candyMachineAccount);
+  console.log('-------------------------------------------------------');
+  console.log('Solaneyees(Wait a sec) =>', 'https://www.solaneyes.com/address/' + base58PublicKey(candyMachine.publicKey));
+}
+
+main();
+
+/*
+Mint twice from Candy Machine example.
+
+% ts-node <THIS FILE>
+
+myKeypair.publicKey => HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg
+collectionUpdateAuthority => 94NVNnGMgLHnLmHDy1oW8MLivo56QXmU1XyNMaDoDgUu
+collectionMint => Hrv61bhATqiVu66WtGyp1diQvLGy7J3WDM5k4VLmZVQS
+candyMachine => 4CVHXRwNcquqrMy7KRZJ94Hwm2adt4dWkSSgtb7TeTjU
+nftMint => DHFrxQZhZ1wrEsFS53BzhUMd7oipDo5xnNiCLfr2tqX3
+-------------------------------------------------------
+candyMachineAccount => {
+  publicKey: {
+    bytes: Uint8Array(32) [
+       47, 132, 110, 120, 127,  28, 189,   5,
+        3, 141,  28, 147, 170,  90, 169, 138,
+       73, 113,  25,  56, 132, 217, 216, 178,
+      226, 124,  53,   8,  91, 224,  37, 111
+    ]
+  },
+  header: {
+    executable: false,
+    owner: { bytes: [Uint8Array] },
+    lamports: { basisPoints: 11825040n, identifier: 'SOL', decimals: 9 },
+    rentEpoch: 0,
+    exists: true
+  },
+  discriminator: [
+    51, 173, 177, 113,
+    25, 241, 109, 189
+  ],
+  version: 1,
+  tokenStandard: 0,
+  features: [ 0, 0, 0, 0, 0, 0 ],
+  authority: {
+    bytes: Uint8Array(32) [
+      245, 164,  74, 111,  54, 131, 150,  17,
+      113,  31,   4,  20, 159,  81, 221,  64,
+      109, 212, 188,  82, 203, 134, 242,  13,
+      210, 177,  22,   8, 166,  44, 126, 233
+    ]
+  },
+  mintAuthority: {
+    bytes: Uint8Array(32) [
+      208, 237,  53,  11, 178, 239, 176,  85,
+       52,  54, 240,   8, 140, 120, 181, 229,
+      174, 182, 245, 113, 115, 190,  14,  33,
+      156, 249, 113, 230, 146, 182,   9,  72
+    ]
+  },
+  collectionMint: {
+    bytes: Uint8Array(32) [
+      250, 132, 121, 181, 247, 214, 169,   1,
+      250, 221, 230,  86,  20,  98,  59, 221,
+       39,  12, 183,  66,  96, 165, 214,  89,
+       39, 146,  77,  42, 194,  42, 149,  95
+    ]
+  },
+  itemsRedeemed: 2n,
+  data: {
+    itemsAvailable: 3n,
+    symbol: '',
+    sellerFeeBasisPoints: { basisPoints: 999n, identifier: '%', decimals: 2 },
+    maxEditionSupply: 0n,
+    isMutable: true,
+    creators: [ [Object] ],
+    configLineSettings: { __option: 'Some', value: [Object] },
+    hiddenSettings: { __option: 'None' }
+  },
+  items: [
+    {
+      index: 0,
+      minted: true,
+      name: 'My NFT #1',
+      uri: 'https://arweave.net/2V5Mx2dwnyrwlpHYPVshiyj_WtU7qIlPZzKs6nIwqw4'
+    },
+    {
+      index: 1,
+      minted: false,
+      name: 'My NFT #2',
+      uri: 'https://arweave.net/2dyAzm_p5kz8GYYLAzKj41KQL3vYT_kWyomJ3mTpBcA'
+    },
+    {
+      index: 2,
+      minted: true,
+      name: 'My NFT #3',
+      uri: 'https://arweave.net/tD5TVplWyzqYvM9feLbIKGqkuTzf0AIO0nLc4NDiiPk'
+    }
+  ],
+  itemsLoaded: 3,
+  ruleSet: { __option: 'None' }
+}
+-------------------------------------------------------
+Solaneyees(Wait a sec) => https://www.solaneyes.com/address/4CVHXRwNcquqrMy7KRZJ94Hwm2adt4dWkSSgtb7TeTjU
+*/
