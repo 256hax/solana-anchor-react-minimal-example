ba05339b437a9a43a8452856f761db017fb4759f
256hax <256hax@gmail.com>
2023-07-01 22:24:23 +0900

    feat: change to demo keypair

--- library/js/base58UsingBs58.ts
+++ library/js/base58UsingBs58.ts
@@ -6,21 +6,16 @@ import {
 // ------------------------------------
 //  Uint8Array to Base58
 // ------------------------------------
-const secretKeyUint8Array: Uint8Array = new Uint8Array([
-  144, 238, 56, 119, 33, 196, 70, 120, 42, 135, 214,
-  122, 86, 168, 224, 206, 106, 31, 92, 161, 155, 164,
-  182, 96, 46, 12, 116, 31, 29, 26, 103, 114, 112,
-  163, 217, 203, 8, 61, 129, 137, 116, 163, 237, 226,
-  118, 79, 92, 255, 12, 125, 182, 109, 174, 41, 131,
-  18, 55, 61, 238, 68, 33, 65, 215, 45
-]);
+const secretKeyUint8Array: Uint8Array = new Uint8Array([42,10,22,97,116,115,107,57,226,247,40,179,216,11,216,9,110,233,110,240,85,78,144,173,253,79,75,12,175,216,43,214,245,164,74,111,54,131,150,17,113,31,4,20,159,81,221,64,109,212,188,82,203,134,242,13,210,177,22,8,166,44,126,233]);
 const toBase58 = bs58.encode(secretKeyUint8Array);
 console.log('toBase58 =>', toBase58);
 
 // ------------------------------------
 //  Base58 to Uint8Array
 // ------------------------------------
-const secretKeyBase58 = '3u4caiG9kSfRSySL9a17tJBUPHdAMkapQrKQeDmHZ9oQeh6LgSKyZMgoicpp9eqZ1Z41Gzom6iputb8b2i9DJweC';
+const secretKeyBase58 = 'qkT6L2d7CY3TP1idkij8UNhzwcfQJfdjvU8NMu4FKHokkPrTeXfhooeeqUsQr5rL8rhZrcroMr4T2CFxanvezgQ';
 // Read KeyPair from secret key.
 const keypair = Keypair.fromSecretKey(bs58.decode(secretKeyBase58));
 console.log('toUint8Array =>', keypair.secretKey);
+
+// PublicKey: HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg
