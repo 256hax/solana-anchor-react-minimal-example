3adea2f7d67b1abcd12e9c590d6dd6a4aca741af
256hax <256hax@gmail.com>
2024-01-08 16:11:38 +0900

    ref: output detailed log

--- scripts/metaplex/bubblegum_CompressedNFT/src/getAssetsByCollection.ts
+++ scripts/metaplex/bubblegum_CompressedNFT/src/getAssetsByCollection.ts
@@ -29,8 +29,9 @@ const getAssetsByGroup = async () => {
     groupValue: collectionMint,
   });
 
-  console.log('collectionMint =>', collectionMint);
-  console.log('assets =>', assets);
+  assets.items.map((a: any) => {
+    console.log('assets =>', a);
+  });
 };
 
 getAssetsByGroup();
@@ -39,48 +40,133 @@ getAssetsByGroup();
 % ts-node src/<THIS_FILE>
 
 ~~~snip~~~
-    {
-      interface: 'V1_NFT',
-      id: 'EbZ3C2Wo9f4RUvi7XHpK554cMo9WNcVZaMUsYx7tKv5L',
-      content: [Object],
-      authorities: [Array],
-      compression: [Object],
-      grouping: [Array],
-      royalty: [Object],
-      creators: [Array],
-      ownership: [Object],
-      supply: [Object],
-      mutable: true,
-      burnt: false
+assets => {
+  interface: 'V1_NFT',
+  id: 'C84LJACyyp6HZSuAnMKZGqzwinzPkynnVpJUMEjy8mrp',
+  content: {
+    '$schema': 'https://schema.metaplex.com/nft1.0.json',
+    json_uri: 'https://nftstorage.link/ipfs/bafkreidk3rfovtx4uehivgp7tmruoiaqkypproymlfzzpgeyayqcbfakma',
+    files: [ [Object], [Object] ],
+    metadata: {
+      attributes: [Array],
+      description: 'Claynosaurz is a collection of 10,000 3D animated NFTs. Helmed by a team of leading animation and game industry artists, Claynosaurz aims to rewrite the narrative on how successful IP can be built out of web3. With massive potential for scaling, the Genesis collection is an all access pass to our Universe.',
+      name: 'cNFT in a Collection',
+      symbol: '',
+      token_standard: 'NonFungible'
     },
-    {
-      interface: 'V1_NFT',
-      id: 'C84LJACyyp6HZSuAnMKZGqzwinzPkynnVpJUMEjy8mrp',
-      content: [Object],
-      authorities: [Array],
-      compression: [Object],
-      grouping: [Array],
-      royalty: [Object],
-      creators: [Array],
-      ownership: [Object],
-      supply: [Object],
-      mutable: true,
-      burnt: false
-    },
-    {
-      interface: 'V1_NFT',
-      id: '4N95uzyVy597TQAVXSccdExkMiFc9gaG3XKNUE2mEL27',
-      content: [Object],
-      authorities: [Array],
-      compression: [Object],
-      grouping: [Array],
-      royalty: [Object],
-      creators: [Array],
-      ownership: [Object],
-      supply: [Object],
-      mutable: true,
-      burnt: false
+    links: {
+      image: 'https://nftstorage.link/ipfs/bafybeibhhh2mnebvajzdai4d7ar42m6s6pkvy7n7smgc6lpj4bp45qvj3y/7315.gif',
+      external_url: 'https://twitter.com/Claynosaurz'
     }
-  ]
+  },
+  authorities: [
+    {
+      address: 'AgGoE8DcTK6nDdy5xo2Pa5GqEpWXwQz8HPJr1C16egnk',
+      scopes: [Array]
+    }
+  ],
+  compression: {
+    eligible: false,
+    compressed: true,
+    data_hash: 'HnnZFpKwwVHAgtTZkvoqvg4f9KK4ecLQJtmD7yHoU9fo',
+    creator_hash: '5MhKaq3eEzsomVp5qU1EST7H9q8R9ndDxWuXeF6wbdgx',
+    asset_hash: 'J6itq8sWXXMoFkNeYMfVhHC1XCvZzYzGed3RBWh44qGA',
+    tree: '4RFxwemYRR9RUDLEH2Uo2EuatUu4EZQsFuEeH7wA8r4f',
+    seq: 2,
+    leaf_id: 1
+  },
+  grouping: [
+    {
+      group_key: 'collection',
+      group_value: 'CNKbk92ugTzDnqZNNttXGWbNmCmHptxctz8BuJYYp9Tx'
+    }
+  ],
+  royalty: {
+    royalty_model: 'creators',
+    target: null,
+    percent: 0.05,
+    basis_points: 500,
+    primary_sale_happened: false,
+    locked: false
+  },
+  creators: [
+    {
+      address: 'HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg',
+      share: 100,
+      verified: true
+    }
+  ],
+  ownership: {
+    frozen: false,
+    delegated: false,
+    delegate: null,
+    ownership_model: 'single',
+    owner: 'HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg'
+  },
+  supply: { print_max_supply: 0, print_current_supply: 0, edition_nonce: null },
+  mutable: true,
+  burnt: false
+}
+assets => {
+  interface: 'V1_NFT',
+  id: '4N95uzyVy597TQAVXSccdExkMiFc9gaG3XKNUE2mEL27',
+  content: {
+    '$schema': 'https://schema.metaplex.com/nft1.0.json',
+    json_uri: 'https://example.com/my-cnft.json',
+    files: [],
+    metadata: {
+      name: 'My Compressed NFT',
+      symbol: '',
+      token_standard: 'NonFungible'
+    },
+    links: {}
+  },
+  authorities: [
+    {
+      address: 'GNfst5RPtrdv4AhnyBH3Zzz3cezUUztjw38TujRSfaE1',
+      scopes: [Array]
+    }
+  ],
+  compression: {
+    eligible: false,
+    compressed: true,
+    data_hash: '9TzVpU79sNHYyDKa4rFfa8ex8t5PMyKPtuGg1h5ag1cp',
+    creator_hash: 'HcPge8XiNdMdQe3Z2kCVtTQD8S81NfXK9mJMu9B5kGqN',
+    asset_hash: 'CfmbJMXTTrgymoqkoRTkDL7pvaTppVhvCMxgWzi56pKw',
+    tree: 'B9bq2sirvRtgDfZdaTqPso3h6ghfWjXfx77CHdWKHEqT',
+    seq: 6,
+    leaf_id: 5
+  },
+  grouping: [
+    {
+      group_key: 'collection',
+      group_value: 'CNKbk92ugTzDnqZNNttXGWbNmCmHptxctz8BuJYYp9Tx'
+    }
+  ],
+  royalty: {
+    royalty_model: 'creators',
+    target: null,
+    percent: 0.05,
+    basis_points: 500,
+    primary_sale_happened: false,
+    locked: false
+  },
+  creators: [
+    {
+      address: 'HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg',
+      share: 100,
+      verified: false
+    }
+  ],
+  ownership: {
+    frozen: false,
+    delegated: false,
+    delegate: null,
+    ownership_model: 'single',
+    owner: 'HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg'
+  },
+  supply: { print_max_supply: 0, print_current_supply: 0, edition_nonce: null },
+  mutable: true,
+  burnt: false
 }
 */
