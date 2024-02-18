f01c520c7586ef3d5d71f577e55f1e9cc20278ae
256hax <256hax@gmail.com>
2024-02-19 00:29:04 +0900

    feat: add transfer spl token

--- anchor/transfer-sol/programs/transfer-sol/src/lib.rs
+++ anchor/transfer-sol/programs/transfer-sol/src/lib.rs
@@ -1,5 +1,6 @@
 // Docs: https://www.quicknode.com/guides/solana-development/anchor/transfer-tokens
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer as SplTransfer};
 
 declare_id!("QHQNpUgEYbzC4TXBnJD591AGfBR8KiFivLtSMycBQ7W");
 
@@ -12,8 +13,11 @@ pub mod transfer_sol {
         let to_account = &ctx.accounts.to;
 
         // Create the transfer instruction
-        let transfer_instruction =
-        anchor_lang::solana_program::system_instruction::transfer(from_account.key, to_account.key, amount);
+        let transfer_instruction = anchor_lang::solana_program::system_instruction::transfer(
+            from_account.key,
+            to_account.key,
+            amount,
+        );
 
         // Invoke the transfer instruction
         anchor_lang::solana_program::program::invoke_signed(
@@ -28,6 +32,24 @@ pub mod transfer_sol {
 
         Ok(())
     }
+
+    pub fn transfer_spl_tokens(ctx: Context<TransferSpl>, amount: u64) -> Result<()> {
+        let destination = &ctx.accounts.to_ata;
+        let source = &ctx.accounts.from_ata;
+        let token_program = &ctx.accounts.token_program;
+        let authority = &ctx.accounts.from;
+
+        // Transfer tokens from taker to initializer
+        let cpi_accounts = SplTransfer {
+            from: source.to_account_info().clone(),
+            to: destination.to_account_info().clone(),
+            authority: authority.to_account_info().clone(),
+        };
+        let cpi_program = token_program.to_account_info();
+
+        token::transfer(CpiContext::new(cpi_program, cpi_accounts), amount)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -38,4 +60,14 @@ pub struct TransferLamports<'info> {
     /// CHECK:
     pub to: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct TransferSpl<'info> {
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub from_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
 }
--- anchor/transfer-sol/tests/transfer-sol.ts
+++ anchor/transfer-sol/tests/transfer-sol.ts
@@ -1,8 +1,19 @@
 // Docs: https://www.quicknode.com/guides/solana-development/anchor/transfer-tokens
+// Solana
+import { Keypair } from '@solana/web3.js';
+import {
+  createMint,
+  createAssociatedTokenAccount,
+  TOKEN_PROGRAM_ID,
+  mintTo,
+} from '@solana/spl-token';
+
+// Anchor
 import * as anchor from '@coral-xyz/anchor';
 import { Program, BN } from '@coral-xyz/anchor';
-import { Keypair } from '@solana/web3.js'
 import { TransferSol } from '../target/types/transfer_sol';
+
+// Lib
 import { assert } from 'chai';
 
 describe('transfer-sol', () => {
@@ -14,7 +25,7 @@ describe('transfer-sol', () => {
 
   const program = anchor.workspace.TransferSol as Program<TransferSol>;
 
-  it('Trasnfer SOL to random taker', async () => {
+  it('Trasnfer SOL(Lamports) to random taker', async () => {
     // Generate keypair for the new account
     const taker = new Keypair();
 
@@ -29,6 +40,10 @@ describe('transfer-sol', () => {
       .signers([payer])
       .rpc();
 
+    console.log('---------------------------------------');
+    console.log('Trasnfer SOL(Lamports) to random taker');
+    console.log('---------------------------------------');
+    console.log('payer =>', payer.publicKey);
     console.log('taker =>', taker.publicKey);
     console.log('signature =>', signature);
 
@@ -39,6 +54,73 @@ describe('transfer-sol', () => {
       newAccountBalance,
       data.toNumber(),
       'The new account should have the transferred lamports'
+    );
+  });
+
+  it('Trasnfer SPL Token to random taker', async () => {
+    // Generate keypairs for the new accounts
+    const taker = new Keypair();
+
+    // Create a new mint and initialize it
+    const mint = await createMint(
+      provider.connection,
+      payer,
+      provider.wallet.publicKey,
+      null,
+      0
+    );
+
+    // Create associated token accounts for the new accounts
+    const payerAta = await createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      mint,
+      provider.wallet.publicKey
+    );
+    const takerAta = await createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      mint,
+      taker.publicKey
+    );
+    // Mint tokens to the 'from' associated token account
+    const mintAmount = 1000;
+    const mintedSignature = await mintTo(
+      provider.connection,
+      payer,
+      mint,
+      payerAta,
+      provider.wallet.publicKey,
+      mintAmount
+    );
+
+    // Send transaction
+    const transferAmount = new BN(500);
+    const transferredSignature = await program.methods
+      .transferSplTokens(transferAmount)
+      .accounts({
+        from: provider.wallet.publicKey,
+        fromAta: payerAta,
+        toAta: takerAta,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([payer])
+      .rpc();
+
+
+    console.log('---------------------------------------');
+    console.log('Trasnfer SPL Token to random taker');
+    console.log('---------------------------------------');
+    console.log('payer =>', payer.publicKey);
+    console.log('taker =>', taker.publicKey);
+    console.log('mintedSignature =>', mintedSignature);
+    console.log('transferredSignature =>', transferredSignature);
+
+    const toTokenAccount = await provider.connection.getTokenAccountBalance(takerAta);
+    assert.strictEqual(
+      toTokenAccount.value.uiAmount,
+      transferAmount.toNumber(),
+      "The 'to' token account should have the transferred tokens"
     );
   });
 });
