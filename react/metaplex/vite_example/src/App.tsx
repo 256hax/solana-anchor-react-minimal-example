e317d032974f92d8442ba2c7a705415042e37018
256hax <256hax@gmail.com>
2024-03-20 19:39:33 +0900

    feat: add wallet adapter

--- react/metaplex/vite_example/src/App.tsx
+++ react/metaplex/vite_example/src/App.tsx
@@ -1,41 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
-import {
-  Metaplex,
-  guestIdentity,
-} from '@metaplex-foundation/js';
-import {
-  Connection,
-  clusterApiUrl,
-  PublicKey
-} from '@solana/web3.js';
+import './App.css';
+import { Wallet } from './pages/wallet';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const getMyNfts = async () => {
-    const connection = new Connection(clusterApiUrl('devnet'));
-
-    const metaplex = Metaplex.make(connection)
-      .use(guestIdentity())
-
-    // const owenerPublicKey = new PublicKey("3sEbhF2jnNs5RB2ohFunmCiywFgHZokLWwSxGGAsmWMd"); // Mainnet
-    const owenerPublicKey = new PublicKey('55AfqEL3TC9mpkDZ63UCgDrzPcMQd5aZtDegfQCWQ5tK'); // Devnet
-    const myNfts = await metaplex
-      .nfts()
-      .findAllByOwner({ owner: owenerPublicKey });
-
-    console.log('e.g. myNfts[1].address =>', myNfts[1].address.toString());
-    console.log('e.g. myNfts[1].name =>', myNfts[1].name);
-    console.log('myNfts =>', myNfts);
-  }
-
   return (
-    <div className="App">
-      <button onClick={getMyNfts}>getMyNfts</button>
-    </div>
-  )
+    <>
+      <Wallet />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ react/metaplex/vite_example/src/pages/sendSOLToRandomAddress.tsx
@@ -0,0 +1,39 @@
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+import { FC, useCallback } from 'react';
+
+export const SendSOLToRandomAddress: FC = () => {
+    const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+
+    const onClick = useCallback(async () => {
+        if (!publicKey) throw new WalletNotConnectedError();
+
+        // 890880 lamports as of 2022-09-01
+        const lamports = await connection.getMinimumBalanceForRentExemption(0);
+
+        const transaction = new Transaction().add(
+            SystemProgram.transfer({
+                fromPubkey: publicKey,
+                toPubkey: Keypair.generate().publicKey,
+                lamports,
+            })
+        );
+
+        const {
+            context: { slot: minContextSlot },
+            value: { blockhash, lastValidBlockHeight }
+        } = await connection.getLatestBlockhashAndContext();
+
+        const signature = await sendTransaction(transaction, connection, { minContextSlot });
+
+        await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
+    }, [publicKey, sendTransaction, connection]);
+
+    return (
+        <button onClick={onClick} disabled={!publicKey}>
+            Send SOL to a random address!
+        </button>
+    );
+};
