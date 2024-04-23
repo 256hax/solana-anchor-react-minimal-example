91dd102ac643525b911a962b576741179ff5b775
256hax <256hax@gmail.com>
2024-04-24 00:12:16 +0900

    ref: formatting

--- scripts/solana/spl-token-v0.3.x/createAddressLookupTable.ts
+++ scripts/solana/spl-token-v0.3.x/createAddressLookupTable.ts
@@ -11,10 +11,6 @@ import {
   AddressLookupTableProgram,
   Connection,
   Keypair,
-  LAMPORTS_PER_SOL,
-  PublicKey,
-  SystemProgram,
-  Transaction,
   TransactionInstruction,
   TransactionMessage,
   VersionedTransaction,
@@ -23,109 +19,104 @@ import {
 dotenv.config();
 
 // -------------------------------
-//  Setup
+//  payer
 // -------------------------------
 const secret = process.env.PAYER_SECRET_KEY;
 if (!secret) throw new Error('secret not found.');
-const SIGNER_WALLET = Keypair.fromSecretKey(new Uint8Array(JSON.parse(secret)));
+const payer = Keypair.fromSecretKey(new Uint8Array(JSON.parse(secret)));
 
-const DESTINATION_WALLET = Keypair.generate();
-//const LOOKUP_TABLE_ADDRESS = new PublicKey(""); // We will add this later
-
+// -------------------------------
+//  RPC
+// -------------------------------
 // Replace with QuickNode RPC in .env file.
 const QUICKNODE_RPC = process.env.ENDPOINT;
 if (!QUICKNODE_RPC) throw new Error('QUICKNODE_RPC not found.');
-const SOLANA_CONNECTION = new Connection(QUICKNODE_RPC);
+const connection = new Connection(QUICKNODE_RPC);
+// const connection = new Connection('https://api.devnet.solana.com'); // <= Too many request error.
+// const connection = new Connection('http://127.0.0.1:8899', 'confirmed'); // <= invalid instruction data error.
 
-// const SOLANA_CONNECTION = new Connection('https://api.devnet.solana.com'); // <= Too many request error.
-// const SOLANA_CONNECTION = new Connection('http://127.0.0.1:8899', 'confirmed'); // <= invalid instruction data error.
-
-
-async function createAndSendV0Tx(txInstructions: TransactionInstruction[]) {
-  // Step 1 - Fetch Latest Blockhash
-  let latestBlockhash = await SOLANA_CONNECTION.getLatestBlockhash('finalized');
-  console.log(
-    '   ✅ - Fetched latest blockhash. Last valid height:',
-    latestBlockhash.lastValidBlockHeight
-  );
-
-  // Step 2 - Generate Transaction Message
-  const messageV0 = new TransactionMessage({
-    payerKey: SIGNER_WALLET.publicKey,
-    recentBlockhash: latestBlockhash.blockhash,
-    instructions: txInstructions,
-  }).compileToV0Message();
-  console.log('   ✅ - Compiled transaction message');
-  const transaction = new VersionedTransaction(messageV0);
-
-  // Step 3 - Sign your transaction with the required `Signers`
-  transaction.sign([SIGNER_WALLET]);
-  console.log('   ✅ - Transaction Signed');
-
-  // Step 4 - Send our v0 transaction to the cluster
-  const txid = await SOLANA_CONNECTION.sendTransaction(transaction, {
-    maxRetries: 5,
-  });
-  console.log('   ✅ - Transaction sent to network');
-
-  // Step 5 - Confirm Transaction
-  const confirmation = await SOLANA_CONNECTION.confirmTransaction({
-    signature: txid,
-    blockhash: latestBlockhash.blockhash,
-    lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-  });
-  if (confirmation.value.err) {
-    throw new Error('   ❌ - Transaction not confirmed.');
-  }
-  console.log(
-    '🎉 Transaction succesfully confirmed!',
-    '\n',
-    `https://explorer.solana.com/tx/${txid}?cluster=devnet`
-  );
-}
-
-
-async function createLookupTable() {
-  // ------------------------------------------------------------------------
-  //  Airdrop
-  // ------------------------------------------------------------------------
-  // const latestBlockhash = await SOLANA_CONNECTION.getLatestBlockhash();
-  // const signatureAirdrop = await SOLANA_CONNECTION.requestAirdrop(
-  //   SIGNER_WALLET.publicKey,
-  //   LAMPORTS_PER_SOL
-  // );
-  // await SOLANA_CONNECTION.confirmTransaction({
-  //   blockhash: latestBlockhash.blockhash,
-  //   lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
-  //   signature: signatureAirdrop,
-  // });
-
+async function createAddressLookupTable() {
   // Step 1 - Get a lookup table address and create lookup table instruction
   const [lookupTableInst, lookupTableAddress] =
     AddressLookupTableProgram.createLookupTable({
-      authority: SIGNER_WALLET.publicKey,
-      payer: SIGNER_WALLET.publicKey,
-      recentSlot: await SOLANA_CONNECTION.getSlot(),
+      authority: payer.publicKey,
+      payer: payer.publicKey,
+      recentSlot: await connection.getSlot(),
     });
 
   // Step 2 - Log Lookup Table Address
   console.log('Lookup Table Address:', lookupTableAddress.toBase58());
 
-  // Step 3 - Generate a transaction and send it to the network
-  createAndSendV0Tx([lookupTableInst]);
+  // Step 3 - Fetch Latest Blockhash
+  let latestBlockhash = await connection.getLatestBlockhash();
+
+  // Step 4 - Generate Transaction Message
+  const messageV0 = new TransactionMessage({
+    payerKey: payer.publicKey,
+    recentBlockhash: latestBlockhash.blockhash,
+    instructions: [lookupTableInst],
+  }).compileToV0Message();
+  const transaction = new VersionedTransaction(messageV0);
+
+  console.log('messageV0 =>', messageV0);
+
+  // Step 5 - Sign your transaction with the required `Signers`
+  transaction.sign([payer]);
+
+  // Step 6 - Send our v0 transaction to the cluster
+  const signature = await connection.sendTransaction(transaction, {
+    maxRetries: 5,
+  });
+
+  // Step 7 - Confirm Transaction
+  const confirmation = await connection.confirmTransaction({
+    signature: signature,
+    blockhash: latestBlockhash.blockhash,
+    lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+  });
+  if (confirmation.value.err) {
+    throw new Error('Transaction not confirmed.');
+  }
+
+  console.log('signature =>', signature);
 }
 
-createLookupTable();
+createAddressLookupTable();
 
 /*
 ts-node createAddressLookupTable.ts
-(node:50253) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
+(node:53799) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
 (Use `node --trace-deprecation ...` to show where the warning was created)
-Lookup Table Address: 9ihS8eqZpuJnSJerjxA8oQQh44bNJe7q1riCFKHR9Nfg
-   ✅ - Fetched latest blockhash. Last valid height: 282293408
-   ✅ - Compiled transaction message
-   ✅ - Transaction Signed
-   ✅ - Transaction sent to network
-🎉 Transaction succesfully confirmed!
- https://explorer.solana.com/tx/5EPr8GVd93tbQsGkpvr9So1KxjuofhJDRjB8iDm8cSj5ygr8814JxaaQF5DiAhibEzv2daui8kERgvmk1q5z7bnF?cluster=devnet
+Lookup Table Address: 7b6pc8wU8VkoS9bB9wX24nf5z9BnyQCD9GZ81j329Fva
+messageV0 => MessageV0 {
+  header: {
+    numRequiredSignatures: 1,
+    numReadonlySignedAccounts: 0,
+    numReadonlyUnsignedAccounts: 2
+  },
+  staticAccountKeys: [
+    PublicKey [PublicKey(HXtBm8XZbxaTt41uqaKhwUAa6Z1aPyvJdsZVENiWsetg)] {
+      _bn: <BN: f5a44a6f36839611711f04149f51dd406dd4bc52cb86f20dd2b11608a62c7ee9>
+    },
+    PublicKey [PublicKey(7b6pc8wU8VkoS9bB9wX24nf5z9BnyQCD9GZ81j329Fva)] {
+      _bn: <BN: 61e2735e1ac9cb62f2627966713b9f2934f352a5c9c657dfbb8fe66d0a36d72b>
+    },
+    PublicKey [PublicKey(AddressLookupTab1e1111111111111111111111111)] {
+      _bn: <BN: 277a6af97339b7ac88d1892c90446f50002309266f62e53c118244982000000>
+    },
+    PublicKey [PublicKey(11111111111111111111111111111111)] {
+      _bn: <BN: 0>
+    }
+  ],
+  recentBlockhash: 'DKYj5rnEsmLht6z8WJouwCrdcgGx392vqbGc8zgJijni',
+  compiledInstructions: [
+    {
+      programIdIndex: 2,
+      accountKeyIndexes: [Array],
+      data: <Buffer 00 00 00 00 75 08 88 11 00 00 00 00 fc>
+    }
+  ],
+  addressTableLookups: []
+}
+signature => KBoDSdEEyZebshbLNJZHZ9FF8kXUpCdMrmmT8sZRGFALGsH6ggdBTZNqYdCz2hjSPs3jvkvmMVyhgXCsiE6wAH8
 */
