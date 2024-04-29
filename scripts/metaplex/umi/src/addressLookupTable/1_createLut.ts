25afa12008dc9cd7bf66e41c39620ea925ed1ba5
256hax <256hax@gmail.com>
2024-04-29 12:03:19 +0900

    ref: remove unused code

--- scripts/metaplex/umi/src/addressLookupTable/1_createLut.ts
+++ scripts/metaplex/umi/src/addressLookupTable/1_createLut.ts
@@ -47,15 +47,6 @@ const main = async () => {
   lutBuilder.setAddressLookupTables([lut]);
 
   // ----------------------------------------------------
-  //  Add Instructions to Address Lookup Table Builder
-  // ----------------------------------------------------
-  const builder = transactionBuilder().add(
-    addMemo(umi, { memo: 'Hello world!' })
-  );
-
-  lutBuilder.add(addMemo(umi, { memo: 'Hello world!' }));
-
-  // ----------------------------------------------------
   //  Send Transaction
   // ----------------------------------------------------
   const result = await lutBuilder.sendAndConfirm(umi);
@@ -63,66 +54,21 @@ const main = async () => {
   console.log('addressA =>', addressA.toString());
   console.log('addressB =>', addressB.toString());
   console.log('lut =>', lut);
-  console.log('lutBuilder => %o', builder);
   console.log('signature =>', bs58.encode(result.signature));
 };
 
 main();
 
 /*
-ts-node src/createLut_addressLookupTable.ts
-(node:4298) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
-(Use `node --trace-deprecation ...` to show where the warning was created)
-Server responded with 429 Too Many Requests.  Retrying after 500ms delay...
-Server responded with 429 Too Many Requests.  Retrying after 1000ms delay...
-Server responded with 429 Too Many Requests.  Retrying after 2000ms delay...
-Server responded with 429 Too Many Requests.  Retrying after 4000ms delay...
-Server responded with 429 Too Many Requests.  Retrying after 500ms delay...
-addressA => B92Z3mx8q1KKnJxE9aZmdCYYLrwkAz8Aj9B7Ws5Q6wwk
-addressB => 99QfPYYCDZGVNSiAJpwA21HFja8c5JrWjKQMJoSdS4Eb
+ts-node src/addressLookupTable/1_createLut.ts
+addressA => AtbyFhLbPQj2gtiRYaEyhdFRnE8WUuwNErY8QrgukFyB
+addressB => 3MXZ3XKY3BTZL7HDTCKGqFdsTJvEpmQwGe1vEK8NWNPN
 lut => {
-  publicKey: 'J133NRdP1P3VLgpYCYcNsS8VTnw1fW7p48626M6rpHyy',
+  publicKey: 'DD6xxSHmvDyBvttrcH2bHgichFCDEkp7fZxuSWcH234e',
   addresses: [
-    'B92Z3mx8q1KKnJxE9aZmdCYYLrwkAz8Aj9B7Ws5Q6wwk',
-    '99QfPYYCDZGVNSiAJpwA21HFja8c5JrWjKQMJoSdS4Eb'
+    'AtbyFhLbPQj2gtiRYaEyhdFRnE8WUuwNErY8QrgukFyB',
+    '3MXZ3XKY3BTZL7HDTCKGqFdsTJvEpmQwGe1vEK8NWNPN'
   ]
 }
-lutBuilder => TransactionBuilder {
-  items: [
-    {
-      instruction: {
-        keys: [ [length]: 0 ],
-        programId: 'Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo',
-        data: Uint8Array(16) [
-          12,
-          0,
-          0,
-          0,
-          72,
-          101,
-          108,
-          108,
-          111,
-          32,
-          119,
-          111,
-          114,
-          108,
-          100,
-          33,
-          [BYTES_PER_ELEMENT]: 1,
-          [length]: 16,
-          [byteLength]: 16,
-          [byteOffset]: 0,
-          [buffer]: ArrayBuffer { byteLength: 16 }
-        ]
-      },
-      signers: [ [length]: 0 ],
-      bytesCreatedOnChain: 0
-    },
-    [length]: 1
-  ],
-  options: {}
-}
-signature => 2CU5oSt9y8WTa2nMgqbyfGDiWEsMm8gf3Uukd6Yf31JiNfM39oPS8jU18ygmByL4MCYDcHney17sdBNHb18qQWge
+signature => 5DSsVBAcoYREuwLwDYTDkrHyozVXUUmj6NrgxvoRvh27cVHLLhSrcMdCLhypNfpkLhvrtPmdzcJSWGkqizu76LEA
 */
