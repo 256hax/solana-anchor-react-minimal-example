796779a889bfcd45f8ec31f5c1defc203de17d35
256hax <256hax@gmail.com>
2024-07-15 15:59:20 +0900

    fix: typo

--- react/connect_wallet/solana-wallet-adapter_without_metaplex/src/components/SignMessageButton.tsx
+++ react/connect_wallet/solana-wallet-adapter_without_metaplex/src/components/SignMessageButton.tsx
@@ -1,14 +1,4 @@
-import React, { FC, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider, useWallet } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { BraveWalletAdapter, MathWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter, TokenPocketWalletAdapter, TrustWalletAdapter, UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {
-	WalletModalProvider,
-	WalletDisconnectButton,
-	WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-
+import { useWallet } from '@solana/wallet-adapter-react';
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
 
